a0643da23c417f500b697d2356b3c1a5a70c970e
DuongTienSinh <huynhnhutduong7112003@gmail.com>
2024-10-12 14:32:43 +0700

    Movie Schedule section, Seat Booking base

---
+++ src/components/SeatBooking/SeatBooking.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SeatBooking = () => {
+  return <div className="bg-red-400 w-[700px] h-[500px] text-center">
+    Hello
+  </div>;
+};
+
+export default SeatBooking;
